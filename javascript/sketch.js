318746d6af0132986ea2c737bb62184b796c818c
Hector Franco <tor6.franco6@hotmail.com>
2019-04-04 20:17:12 -0600

    Added description functionality

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,13 +1,14 @@
 var globalAnswer = []
 // My own functions
 class Box {
-  constructor (posX, posY, answer) {
+  constructor (posX, posY, answer, description) {
     this.x = posX
     this.y = posY
     this.width = 80
     this.height = 25
     this.answer = answer
     this.hasBeenAnswered = false
+    this.description = description
     globalAnswer.push(this.answer)
   }
   draw () {
@@ -30,10 +31,10 @@ class Box {
 // My variables
 var IMG = 'img/test.jpg'
 var boxesOriginal = [
-  new Box(20, 20, 'Hola'),
-  new Box(400, 10, 'Bye'),
-  new Box(200, 80, 'Siiii'),
-  new Box(300, 150, 'Jejeje')
+  new Box(20, 20, 'Hola', 'Esta es una descripcion hola'),
+  new Box(400, 10, 'Bye', 'Esta es una descripcion bye'),
+  new Box(200, 80, 'Siiii', 'Esta es una descripcion siii'),
+  new Box(300, 150, 'Jejeje', 'Esta es una descripcion jejeje')
 ]
 var boxesCopy = boxesOriginal.slice(0, boxesOriginal.length)
 var imchoosing = false
@@ -55,11 +56,15 @@ function draw () {
       boxesOriginal[i].draw()
     }
     if (boxesCopy.length > 0) {
-      actualchoosing = boxesCopy.pop().answer
+      let boxesCopyInstance = boxesCopy.pop()
+      actualchoosing = boxesCopyInstance.answer
+      document.getElementById('actualPart').innerHTML = actualchoosing
+      document.getElementById('partDescription').innerHTML = boxesCopyInstance.description
     } else {
-      document.getElementById('notice').innerHTML = 'GANASTE'
+      document.getElementById('notice').innerHTML = '¡¡¡GANASTE!!!'
+      document.getElementById('actualPart').style.display = 'none'
+      document.getElementById('partDescription').style.display = 'none'
     }
-    document.getElementById('actualPart').innerHTML = actualchoosing
     imchoosing = true
   } else {
     if (mouseIsPressed) {
